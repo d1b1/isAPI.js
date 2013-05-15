921e7e540ec3e517e90ff2eae9a03197bedcafda
Stephan Smith <stephan.smith.bc93@gmail.com>
2013-05-15 00:30:48 -0400

    Added basic AOuth Signing. Changed the content-type def. Misc

--- example.js
+++ example.js
@@ -1,47 +1,78 @@
 var assert   = require('assert');
 var _        = require('underscore');
-var api      = require('./lib/isapi').Base;
+var api      = require('./lib/isapi').APIClient;
 
-api.host('staging-api.formagg.io');
+// api.host('staging-api.formagg.io');
+
+api.host('localhost');
+api.port(4001)
+
+GLOBAL.spacedInfo = {};
+
+// Define the auth values for signing requests.
+api.configAuth( { 
+  consumer_key:    'abc123', 
+  consumer_secret: 'ssh-secret', 
+  token:           '3H3Mw4Oxi5vDmujM', 
+  token_secret:    'nF7duFct2qTnZpPKcvGabJJwS2lU3zYng0cz1OCRtJgdOb4elhEJkol7j3OTSHoR' 
+});
+
+// Define the auth callback function.
+api.auth_cb( require('./lib/isapi').authorization.sign );
 
 describe('/relationship', function(done) {   
 
-  it('should retreive a maker', function(done){
-    _.clone(api)
-      .path('/maker/510df090da57f2000000011e')
-      .json()
-      .get()
-      .debug(false)
-      .assertions(
-        { 
-          '$.name': function(val) { assert.equal(val, 'Lactalis') } 
-        }
-      )
-      .done(done);
-    }
-  );
+  // it('should retreive a maker', function(done){
+  //   _.clone(api)
+  //     .path('/maker/5192f988add2886f2a000002')
+  //     .json()
+  //     .get()
+  //     .authorize()
+  //     .debug(false)
+  //     .assertions(
+  //       { 
+  //         '$.data.name': function(val) { assert.equal(val, 'asdadsfasdf') },
+  //         '$.data._id': function(val) { assert.equal(val, '5192f988add2886f2a000002') }, 
+  //       }
+  //     )
+  //     .remember( 'NewMaker' )
+  //     .done(done);
+  //   }
+  // );
 
   it('should give us the new maker', function(done){
 
     _.clone(api)
       .path('/maker')
-        .query('a=111')
-        .query( [ 't=11', 'r4=222'])
-        .query('b=111')
       .json()
+      .authorize()
       .post( 
         {
-          name: 'This little test',
-          source: 'cow',
-          texture: 'Blue'
+          "name": "My Test Maker"
         }
       )
-      .debug(true)
+      .debug(false)
       .assertions(
         { 
-          '$.statusCode': function(val) { assert.equal(val, 401) },
-          '$.data': function(val) { assert.equal(val, 'Unauthorized') },
-          '$.raw': function(val) { assert.equal(val, '111') }
+          '$.statusCode': function(val) { assert.equal(val, 200) },
+        }
+      )
+      .remember( 'NewMaker' )
+      .done(done);
+    }
+  );
+
+  it('should retreive a maker', function(done){
+    _.clone(api)
+      .path('/maker/' + GLOBAL.spacedInfo.NewMaker._id)
+      .json()
+      .get()
+      .authorize()
+      .debug(false)
+      .assertions(
+        { 
+          '$.data.name': function(val) { assert.equal(val, GLOBAL.spacedInfo.NewMaker.name ) },
+          '$.data._id': function(val) { assert.equal(val, GLOBAL.spacedInfo.NewMaker._id ) }, 
         }
       )
       .done(done);
