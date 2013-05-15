26d6d37aecdb0d3f4a7a33db56b07031ea9f7389
Stephan Smith <stephan.smith.bc93@gmail.com>
2013-05-15 15:28:29 -0400

    Removed all cruft files

--- test/blank.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-
-// Simple Placeholder mocha test for getting started with travis.
-
-describe('/isAPI', function() {   
- it('1 is 1', function(){
-   assert.equal(1, 1);
- });
-});
